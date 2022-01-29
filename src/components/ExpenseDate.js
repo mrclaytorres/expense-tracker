f9a897229c14761117c47b5bd70cc10b2f7e3ca2
Algen <adelacruz@innovuze.com>
2022-01-29 10:16:30 +0800

    Adding Expenses component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,16 @@
+import './ExpenseDate.css';
+
 function ExpenseDate(props) {
+
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
-    <div>
-      <div>{ props.month }</div>
-      <div>{ props.day }</div>
-      <div>{ props.year }</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,14 +2,11 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
       <div>
-        <ExpenseDate month={month} day={day} year={year} />
+        <ExpenseDate date={ props.date } />
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
