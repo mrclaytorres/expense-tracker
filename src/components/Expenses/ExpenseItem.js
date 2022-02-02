99c8285aefa3954fb3e14417634381db4be2d65e
Algen <adelacruz@innovuze.com>
2022-02-02 22:33:12 +0800

    Adding conditional editing form button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    
   return (
-    <Card className="expense-item">
-      <div>
-        <ExpenseDate date={ props.date } />
-      </div>
-      <div className="expense-item__description">
-        <h2>{ props.title }</h2>
-        <div className="expense-item__price">${ props.amount }</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <div>
+          <ExpenseDate date={props.date} />
+        </div>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
