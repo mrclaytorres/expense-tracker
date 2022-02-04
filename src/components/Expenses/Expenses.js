2ee9003af4f32fd4fd7bc73bb36c3355cd800b1d
Algen <adelacruz@innovuze.com>
2022-02-04 16:26:13 +0800

    Adding Chart bars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
-
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -30,6 +30,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeYear={changeYearHandler}
       />
+      <ExpensesChart expenses={filteredYearArray}/>
       <ExpensesList items={filteredYearArray} />
     </Card>
   );
